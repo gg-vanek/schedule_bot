ddf3d5410b7b8a8e7eb8636d0dfcf554406f3e5a
lvanekl <23.gorjachev.i@gmail.com>
2021-12-20 14:52:16 +0300

    Add bot code, data processer and db

---
+++ bot.py
@@ -0,0 +1,349 @@
+import logging
+import aiogram
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+import sqlite3
+from aiogram.dispatcher.webhook import SendMessage
+from aiogram.dispatcher.filters import Text
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
+import json
+import datetime
+
+# http://t.me/liceum_schedule_bot
+bot_token = ''
+bot = Bot(bot_token)
+dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
+
+WEBHOOK_PORT = 5000
+WEBHOOK_HOST = "127.0.0.1"
+WEBHOOK_URL = 'https://f610-46-73-138-171.ngrok.io'
+WEBHOOK_PATH = ''
+WEBAPP_PORT = 5000
+
+grade = 0
+letter = ''
+group = 0
+full_class = ''
+lessons = []
+command = ''
+
+db = sqlite3.connect('bot.db')
+sql = db.cursor()
+sql.execute("""CREATE TABLE IF NOT EXISTS users_data (
+    id INT,
+    class   TEXT)
+    """)
+
+
+# ------------------START--------
+@dp.message_handler(commands=['start'])
+async def welcome(message: types.Message):
+    global command
+    command = 'start'
+    await bot.send_message(message.chat.id, 'Привет, лицеист')
+    create_grade_buttons()
+    await bot.send_message(message.chat.id, 'Сейчас от тебя требуется выбрать класс, в котором ты учишься')
+    await bot.send_message(message.chat.id, 'Выбери класс', reply_markup=grade_buttons)
+
+
+# ------------------RESET_CLASS---------
+@dp.message_handler(commands=['reset_grade'])
+async def reset_grade(message: types.Message):
+    global command
+    command = 'reset_grade'
+    create_grade_buttons()
+    await bot.send_message(message.chat.id, 'Выбери класс', reply_markup=grade_buttons)
+
+
+# ------------------ONE_DAY_LESSONS--------
+@dp.message_handler(commands=['one_day_lessons'])
+async def one_day_lessons(message: types.Message):
+    global grade_buttons, command
+    rows_id = sql.execute('''SELECT * FROM users_data''').fetchall()
+    print(rows_id)
+    command = 'one_day_lessons'
+    create_grade_buttons()
+    await bot.send_message(message.chat.id, 'Выбери класс', reply_markup=grade_buttons)
+
+
+# --------------------ALL_WEEK_LESSONS-----------------
+
+@dp.message_handler(commands=['all_week_lessons'])
+async def schedule(message: types.Message):
+    global grade_buttons, command
+    command = 'all_week_lessons'
+    create_grade_buttons()
+    await bot.send_message(message.chat.id, 'Выбери класс', reply_markup=grade_buttons)
+
+
+# ------------------TODAY_LESSONS--------
+@dp.message_handler(commands=['today_lessons'])
+async def today_lessons(message: types.Message):
+    global lessons
+    today = datetime.datetime.today()
+    weekday = today.weekday()
+
+    rows_id = sql.execute('''SELECT id FROM users_data''').fetchall()
+    rows_idnew = []
+    for i in rows_id:
+        rows_idnew.append(i[0])
+
+    rows_class = sql.execute('''SELECT class FROM users_data''').fetchall()
+    rows_classnew = []
+    for i in rows_class:
+        rows_classnew.append(i[0])
+
+    for i in range(len(rows_idnew)):
+        if rows_idnew[i] == message.chat.id:
+            my_class = rows_classnew[i]
+
+    if weekday == 0:
+        weekday = 'Понедельник'
+    elif weekday == 1:
+        weekday = 'Вторник'
+    elif weekday == 2:
+        weekday = 'Среда'
+    elif weekday == 3:
+        weekday = 'Четверг'
+    elif weekday == 4:
+        weekday = 'Пятница'
+    elif weekday == 5:
+        weekday = 'Суббота'
+    elif weekday == 6:
+        await bot.send_message(message.chat.id, 'Сегодня уроков нет, отдыхаем')
+    if weekday != 6:
+        open_schedule(my_class, weekday)
+        await bot.send_message(message.chat.id, lessons[0])
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # print(message)
+    global letter_buttons, grade, letter, group, full_class, lessons, command, days_buttons
+    if message.text == '8':
+        create_letter_button(8)
+        grade = 8
+        await bot.send_message(message.chat.id, 'Выбери букву класса', reply_markup=letter_buttons)
+    if message.text == '9':
+        create_letter_button(9)
+        grade = 9
+        await bot.send_message(message.chat.id, 'Выбери букву класса', reply_markup=letter_buttons)
+    if message.text == '10':
+        create_letter_button(10)
+        grade = 10
+        await bot.send_message(message.chat.id, 'Выбери букву класса', reply_markup=letter_buttons)
+    if message.text == '11':
+        await bot.send_message(message.chat.id, 'походу 11и1 и 11и2 идут нахуй')
+        create_letter_button(11)
+        grade = 11
+        await bot.send_message(message.chat.id, 'Выбери букву класса', reply_markup=letter_buttons)
+
+    if message.text == 'А':
+        letter = 'А'
+        create_group_buttons()
+        await bot.send_message(message.chat.id, 'Выбери группу', reply_markup=group_buttons)
+    if message.text == 'Б':
+        letter = 'Б'
+        create_group_buttons()
+        await bot.send_message(message.chat.id, 'Выбери группу', reply_markup=group_buttons)
+    if message.text == 'В':
+        letter = 'В'
+        create_group_buttons()
+        await bot.send_message(message.chat.id, 'Выбери группу', reply_markup=group_buttons)
+    if message.text == 'Г':
+        letter = 'Г'
+        create_group_buttons()
+        await bot.send_message(message.chat.id, 'Выбери группу', reply_markup=group_buttons)
+    if message.text == 'И':
+        letter = 'И'
+        create_group_buttons()
+        await bot.send_message(message.chat.id, 'Выбери группу', reply_markup=group_buttons)
+
+    if message.text == '1':
+        group = 1
+        full_class = f'{grade}{letter}{group}'
+        if command == 'all_week_lessons':
+            open_schedule(full_class)
+            for i in lessons:
+                await bot.send_message(message.chat.id, i)
+        elif command == 'one_day_lessons':
+            create_days_button()
+            await bot.send_message(message.chat.id, 'Выбери день недели', reply_markup=days_buttons)
+        elif command == 'start':
+            insert_id(message.chat.id, full_class)
+            await bot.send_message(message.chat.id, f'Ты учишься в {full_class} классе')
+        elif command == 'reset_grade':
+            reset_class(message.chat.id, full_class)
+            await bot.send_message(message.chat.id, f'Теперь ты учишься в {full_class} классе')
+
+    if message.text == '2':
+        group = 2
+        full_class = f'{grade}{letter}{group}'
+        if command == 'all_week_lessons':
+            open_schedule(full_class)
+            await bot.send_message(message.chat.id, f'Ты видишь расписани {grade}{letter}{group} класса')
+            for i in lessons:
+                await bot.send_message(message.chat.id, i)
+        elif command == 'one_day_lessons':
+            create_days_button()
+            await bot.send_message(message.chat.id, 'Выбери день недели', reply_markup=days_buttons)
+        elif command == 'start':
+            insert_id(message.chat.id, full_class)
+            await bot.send_message(message.chat.id, f'Ты учишься в {grade}{letter}{group} классе')
+        elif command == 'reset_grade':
+            reset_class(message.chat.id, full_class)
+            await bot.send_message(message.chat.id, f'Теперь ты учишься в {full_class} классе')
+
+    if message.text == 'Monday':
+        open_schedule(full_class, 'Понедельник')
+        await bot.send_message(message.chat.id, f'Ты видишь расписани {grade}{letter}{group} класса на понедельник')
+        await bot.send_message(message.chat.id, lessons[0])
+    if message.text == 'Tuesday':
+        open_schedule(full_class, 'Вторник')
+        await bot.send_message(message.chat.id, f'Ты видишь расписани {grade}{letter}{group} класса на вторник')
+        await bot.send_message(message.chat.id, lessons[0])
+    if message.text == 'Wednesday':
+        open_schedule(full_class, 'Среда')
+        await bot.send_message(message.chat.id, f'Ты видишь расписани {grade}{letter}{group} класса на среду')
+        await bot.send_message(message.chat.id, lessons[0])
+    if message.text == 'Thursday':
+        open_schedule(full_class, 'Четверг')
+        await bot.send_message(message.chat.id, f'Ты видишь расписани {grade}{letter}{group} класса на четверг')
+        await bot.send_message(message.chat.id, lessons[0])
+    if message.text == 'Friday':
+        open_schedule(full_class, 'Пятница')
+        await bot.send_message(message.chat.id, f'Ты видишь расписани {grade}{letter}{group} класса на пятницу')
+        await bot.send_message(message.chat.id, lessons[0])
+    if message.text == 'Saturday':
+        open_schedule(full_class, 'Суббота')
+        await bot.send_message(message.chat.id, f'Ты видишь расписани {grade}{letter}{group} класса на субботу')
+        await bot.send_message(message.chat.id, lessons[0])
+
+
+# кнопки с выбором класса
+def create_grade_buttons():
+    global grade_buttons
+    grade_buttons = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    button8 = KeyboardButton(text='8')
+    button9 = KeyboardButton(text='9')
+    button10 = KeyboardButton(text='10')
+    button11 = KeyboardButton(text='11')
+    grade_buttons.row(button8, button9)
+    grade_buttons.row(button10, button11)
+
+
+# нопки с буквами
+def create_letter_button(grade):
+    global letter_buttons
+    letter_buttons = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    but1 = KeyboardButton(text="А")
+    but2 = KeyboardButton(text='Б')
+    but3 = KeyboardButton(text='В')
+    but4 = KeyboardButton(text='Г')
+    but5 = KeyboardButton(text='И')
+    if grade == 8:
+        letter_buttons.add(but1, but2, but3)
+    elif grade == 9:
+        letter_buttons.add(but1, but2, but3, but4)
+    elif grade == 10 or grade == 11:
+        letter_buttons.add(but1, but2, but3, but4, but5)
+    return letter_buttons
+
+
+def create_group_buttons():
+    global group_buttons
+    group_buttons = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    first_gr = KeyboardButton(text='1')
+    second_gr = KeyboardButton(text='2')
+    group_buttons.add(first_gr, second_gr)
+    return group_buttons
+
+
+def create_days_button():
+    global days_buttons
+    days_buttons = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    Monday = KeyboardButton(text='Monday')
+    Tuesday = KeyboardButton(text='Tuesday')
+    Wednesday = KeyboardButton(text='Wednesday')
+    Thursday = KeyboardButton(text='Thursday')
+    Friday = KeyboardButton(text='Friday')
+    Saturday = KeyboardButton(text='Saturday')
+    days_buttons.add(Monday, Tuesday, Wednesday, Thursday, Friday, Saturday)
+    return days_buttons
+
+
+# вытаскивание информации
+def open_schedule(key, day=None):
+    with open('data/json/schedule.json', 'r') as f:
+        a = json.load(f)
+    if day == None:
+        day_ = 0
+        for i in a[key]:
+            lessons.append(i)
+            lessons[day_] += '\n'
+
+            for j in a[key][i]:
+                if type(a[key][i][j]["Предмет"]) != float:
+                    str = f'{a[key][i][j]["Время"]}  {a[key][i][j]["Предмет"]}  {a[key][i][j]["Кабинет"]}'
+                    lessons[day_] += str
+                    lessons[day_] += '\n'
+            day_ += 1
+    else:
+        lessons.append(day)
+        lessons[0] += '\n'
+        for i in a[key][day]:
+            if type(a[key][day][i]["Предмет"]) != float:
+                str = f'{a[key][day][i]["Время"]}  {a[key][day][i]["Предмет"]}  {a[key][day][i]["Кабинет"]}'
+                lessons[0] += str
+                lessons[0] += '\n'
+    return lessons
+
+
+# add id in database
+def insert_id(id, grade):
+    rows_id = sql.execute('''SELECT id FROM users_data''').fetchall()
+    rows_idnew = []
+    for i in rows_id:
+        rows_idnew.append(i[0])
+    if id not in rows_idnew:
+        sql.execute(f"""INSERT INTO users_data(id, class) VALUES (?, ?)""", (id, grade))
+    db.commit()
+
+
+# reset class in db
+def reset_class(id, grade):
+    sql.execute(f'''UPDATE users_data SET class = ? WHERE id = ?''', (grade, id))
+    db.commit()
+
+
+# ------------------webhook------------------------------------------------------------------------
+
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+
+
+async def on_shutdown(dp):
+    logging.warning('Shutting down..')
+
+    # insert code here to run it before shutdown
+
+    # Remove webhook (not acceptable in some cases)
+    await bot.delete_webhook()
+
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+    logging.warning('Bye!')
+
+
+if __name__ == '__main__':
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBHOOK_HOST,
+        port=WEBAPP_PORT,
+    )
---
+++ download_convert_clear_data.py
@@ -0,0 +1,208 @@
+import requests
+import regex as re
+import tabula
+import pandas as pd
+import glob
+import numpy as np
+import json
+
+
+def download():
+    # скачивает файл с сайта и возвращает его имя в формате типа такого "*** Расписание *** .pdf"
+    url = 'https://www.1511.ru/info/study/40'
+
+    pattern = '<a href="(.*?)".*?>(.*?Расписание учеников.pdf.*?)</a>'
+
+    site = requests.get(url).text
+
+    match_obj = re.search(pattern, site)
+    file_name = match_obj[2]
+    timetable_url = match_obj[1]
+
+    # print(requests.get('https://www.1511.ru' + timetable_url).text)
+    with open('data/pdf/' + file_name, 'wb') as f:
+        f.write(requests.get('https://www.1511.ru' + timetable_url).content)
+    return file_name
+
+
+def convert():
+    pdf_files = glob.glob('data/pdf/*.pdf')
+
+    pdf_pages = tabula.read_pdf(pdf_files[-1], pages='all', multiple_tables=True, pandas_options={'header': None})
+
+    # start1, end1, start2, end2 = map(int, input("Введите (start1, end1, start2, end2)").split())
+    start1, end1, start2, end2 = 31, 37, 38, 43
+
+    last_start1, last_end1, last_start2, last_end2, last_start3, last_end3 = 1, 2, 3, 28, 29, 35
+
+    timetable = {}
+    for page in pdf_pages:
+        if page.shape == (44, 17):
+            page = pd.concat([page.iloc[:, 0:5], page.iloc[:, 0], page.iloc[:, 5:]], axis='columns')
+            page.iloc[0, 4], page.iloc[0, 5] = np.NaN, page.iloc[0, 4]
+            page.columns = range(0, 18)
+
+        if page.shape == (44, 18):
+            # сктроки с start1 по end1 включительно сдвинуть на 1 ячейку вправо
+            # строки с start2 по end2 сдвинуть на 2 ячейки в право
+            first_part = page.iloc[0:start1]  # для 0-start1 оставим также
+
+            # для start1-end1+1 отрежем последний столбец и приканкатим в начало
+            second_part = pd.concat([page.iloc[start1:end1 + 1, -1:], page.iloc[start1:end1 + 1, 0:-1]], axis=1)
+
+            # для start2-end2+1 отрежем 2 столбца в конце и приканкатим в начало
+            third_part = pd.concat([page.iloc[start2:end2 + 1, -2:], page.iloc[start2:end2 + 1, 0:-2]], axis=1)
+
+            # сделаем одинаковые названия колонок
+            second_part.columns = first_part.columns
+            third_part.columns = first_part.columns
+
+            # склеим получим чистую страницу
+            clear_page = pd.concat([first_part, second_part, third_part], axis=0)
+
+            # далее еще первая строка странн опрочиталась, ее нужно полностью переделать
+            # получим номера классов которые есть на этой странице
+            page_classes = clear_page.iloc[0].values
+            page_classes = [item for item in page_classes if not (pd.isnull(item))]
+
+            # ['Дн', 'Урок', '8А1', '8А2', '8Б1', '8Б2']
+            # они выглядят вот так поэтому возьмем срез [2:]
+            page_classes = page_classes[2:]
+
+            first_row = [np.NaN, np.NaN, np.NaN, 'Урок', 'Время',
+                         page_classes[0], 'Предмет', 'Кабинет',
+                         page_classes[1], 'Предмет', 'Кабинет',
+                         page_classes[2], 'Предмет', 'Кабинет',
+                         page_classes[3], 'Предмет', 'Кабинет',
+                         np.NaN]
+
+            clear_page.iloc[0] = first_row
+
+            # далее удалим лишние стобцы
+            clear_page = clear_page.drop([0, 1, 2, 17], axis='columns').drop([1], axis='index')
+
+        if page.shape == (44, 10):
+            # сктроки с start1 по end1 включительно сдвинуть на 3 ячейку влево
+            # строки с start2 по end2 сдвинуть на 2 ячейки в право
+            first_part = page.iloc[0:1]  # для 0-start1 оставим также
+
+            # для start1-end1+1 отрежем последний столбец и приканкатим в начало
+            second_part = pd.concat(
+                [page.iloc[last_start1:last_end1 + 1, 3:], page.iloc[last_start1:last_end1 + 1, :3]],
+                axis=1)
+
+            # для start2-end2+1 отрежем 2 столбца в конце и приканкатим в начало
+            third_part = pd.concat([page.iloc[last_start2:last_end2 + 1, 2:], page.iloc[last_start2:last_end2 + 1, :2]],
+                                   axis=1)
+
+            # для start2-end2+1 отрежем 2 столбца в конце и приканкатим в начало
+            forth_part = pd.concat([page.iloc[last_start3:last_end3 + 1, 1:], page.iloc[last_start3:last_end3 + 1, :1]],
+                                   axis=1)
+
+            fith_part = page.iloc[last_end3 + 1:, :]
+            # сделаем одинаковые названия колонок
+            second_part.columns = first_part.columns
+            third_part.columns = first_part.columns
+            forth_part.columns = first_part.columns
+            fith_part.columns = first_part.columns
+
+            clear_page = pd.concat([first_part, second_part, third_part, forth_part, fith_part], axis=0)
+
+            clear_page = pd.concat([clear_page.iloc[:, 0:2], clear_page.iloc[:, -1], clear_page.iloc[:, 2:]],
+                                   axis='columns')
+            clear_page.columns = range(0, 11)
+
+            page_classes = ['11И1', "11И2"]
+
+            clear_page.iloc[0] = ['Урок', 'Время',
+                                  page_classes[0], 'Предмет', 'Кабинет',
+                                  page_classes[1], 'Предмет', 'Кабинет', np.NaN, np.NaN, np.NaN]
+
+            clear_page = clear_page.drop([8, 9, 10], axis='columns').drop([1], axis='index')
+
+        # далее тактика такая: отступаем 2 столбца
+        # берем 3 следующих стобца (класс, предмет, кабинет)
+        # приклеиваем их к предыдущим двум
+        # записываем в словарь
+        # {класс1: {день1: список уроков, день2: список уроков ...}}, класс2: {...}}
+
+        class_column_groups = [[2, 3, 4], [5, 6, 7]]
+        if len(page_classes) == 4:
+            class_column_groups.append([8, 9, 10])
+            class_column_groups.append([11, 12, 13])
+
+        for class_columns in class_column_groups:
+            single_class_timetable_df = clear_page.iloc[:, [0, 1] + class_columns]
+            single_class_timetable_dict = {'Понедельник': {1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}, 7: {}},
+                                           'Вторник': {1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}, 7: {}},
+                                           'Среда': {1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}, 7: {}},
+                                           'Четверг': {1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}, 7: {}},
+                                           'Пятница': {1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}, 7: {}},
+                                           'Суббота': {1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}, 7: {}}}
+
+            iterations_dict = [list(range(1, 8)),
+                               list(range(8, 15)),
+                               list(range(15, 22)),
+                               list(range(22, 29)),
+                               list(range(29, 36)),
+                               list(range(36, 43))]
+            for iteration_day_id, day in enumerate(single_class_timetable_dict):
+                for i in range(1, 8):
+                    single_class_timetable_dict[day][i]["Время"] = single_class_timetable_df.iloc[
+                        iterations_dict[iteration_day_id][i - 1], 1]
+                    single_class_timetable_dict[day][i]["Предмет"] = single_class_timetable_df.iloc[
+                        iterations_dict[iteration_day_id][i - 1], -2]
+                    single_class_timetable_dict[day][i]["Кабинет"] = single_class_timetable_df.iloc[
+                        iterations_dict[iteration_day_id][i - 1], -1]
+            timetable[single_class_timetable_df.iloc[0, 2]] = single_class_timetable_dict
+
+    exit_file_name = 'data/json/' + pdf_files[-1].split('\\')[-1][:-4] + '.json'
+    # clear_data(timetable)
+    with open(exit_file_name, 'w', encoding="cp1251") as f:
+        json.dump(timetable, f)
+
+    return exit_file_name
+
+
+def clear_data(path):
+    with open(path, 'r') as f:
+        timetable = json.load(f)
+
+    week_days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
+    time_subj_room_keys = ['Время', 'Предмет', 'Кабинет']
+
+    for class_name in ['11И1', '11И2']:
+        t = timetable[class_name]
+        for n, i in enumerate(week_days[:-1]):
+            times = t[i]['7'][time_subj_room_keys[0]].split('\r')
+            subjects = t[i]['7'][time_subj_room_keys[1]].split('\r')
+            classrooms = t[i]['7'][time_subj_room_keys[2]].split('\r')
+            if len(times) == 3:
+                times = [times[0]] + [times[2]]
+                subjects = [np.NaN] + [subjects[1]]
+            if len(subjects) == 1:
+                subjects = [np.NaN] + [subjects[0]]
+            if len(classrooms) == 1:
+                classrooms = [np.NaN] + [classrooms[0]]
+            if len(times) == 2:
+                t[i]['7'][time_subj_room_keys[0]] = times[0]
+                t[i]['7'][time_subj_room_keys[1]] = subjects[0]
+                t[i]['7'][time_subj_room_keys[2]] = classrooms[0]
+
+                t[week_days[n + 1]]['1'][time_subj_room_keys[0]] = times[1]
+                t[week_days[n + 1]]['1'][time_subj_room_keys[1]] = subjects[1]
+                t[week_days[n + 1]]['1'][time_subj_room_keys[2]] = classrooms[1]
+    with open(path, 'w') as f:
+        json.dump(timetable, f)
+
+
+def main():
+    download()
+    exit_file_name = convert()
+    clear_data(exit_file_name)
+
+    return exit_file_name
+
+
+if __name__ == '__main__':
+    main()
